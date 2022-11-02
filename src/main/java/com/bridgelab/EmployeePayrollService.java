744a1600529d27abc3eb26f76715a655dce78e41
nitis <nitish8152@gmail.com>
2022-11-02 10:48:55 +0530

    Welcome To Employee Payroll Service

---
+++ src/main/java/com/bridgelab/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelab;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println(".....Welcome To Employee Payroll Services.....");
+    }
+}
